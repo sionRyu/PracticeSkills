819ffe37ae6fd18586d49caf6013013f466c0ef2
sionRyu <alpems15@sogang.ac.kr>
2021-12-21 16:52:04 +0900

    networkPractice1,2

---
+++ NetworkPractice/NetworkPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetworkPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
