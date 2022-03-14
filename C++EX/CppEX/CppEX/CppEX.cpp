dcd8a0a92776bfcba1917c291155def40c8f5916
sionRyu <84768651+sionRyu@users.noreply.github.com>
2022-03-14 17:29:30 +0900

    del

--- C++EX/CppEX/CppEX/CppEX.cpp
+++ C++EX/CppEX/CppEX/CppEX.cpp
@@ -1,8 +1,42 @@
-﻿#include <iostream>
+﻿#include<iostream>
+
+using namespace std;
+long int arr[1000000];
 
 int main()
 {
-    int a=0, b=0, c=0;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    std::cout << 
+
+	long int all = 0, rep = 0;
+
+	cin >> all;
+
+
+	for (long int i = 0; i < all; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < all-1; i++)
+	{	
+		if (arr[0]>arr[i+1])
+		{
+			rep = arr[0];
+			arr[0] = arr[i];
+			arr[i] = rep;
+		}
+
+		if (arr[all-1] < arr[i])
+		{
+			rep = arr[all-1];
+			arr[all - 1] = arr[i];
+			arr[i] = rep;
+		}
+	}
+
+	cout << arr[0] <<" "<< arr[all - 1] << endl;
+
+	return 0;
 }
